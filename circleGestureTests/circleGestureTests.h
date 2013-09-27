5114eebd07ac97223f2c78db3959c28724372ab1
Stephen Kyles <BlueOwl@Blues-MacBook-Pro.local>
2013-09-26 21:16:58 -0700

    Initial Application (0.1)

---
+++ circleGestureTests/circleGestureTests.h
@@ -0,0 +1,13 @@
+//
+//  circleGestureTests.h
+//  circleGestureTests
+//
+//  Created by Stephen Kyles on 9/26/13.
+//  Copyright (c) 2013 Blue Owl Labs. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface circleGestureTests : SenTestCase
+
+@end
---
+++ circleGestureTests/circleGestureTests.m
@@ -0,0 +1,32 @@
+//
+//  circleGestureTests.m
+//  circleGestureTests
+//
+//  Created by Stephen Kyles on 9/26/13.
+//  Copyright (c) 2013 Blue Owl Labs. All rights reserved.
+//
+
+#import "circleGestureTests.h"
+
+@implementation circleGestureTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in circleGestureTests");
+}
+
+@end
